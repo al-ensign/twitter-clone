ca7c73c953396cdd39543025f8058b3ae6dc3232
alensign <alexandra.wilchitskaya@gmail.com>
2022-09-12 18:51:33 +0300

    Removed conf

--- stats_microservice/repository.py
+++ stats_microservice/repository.py
@@ -16,11 +16,6 @@ logger = logging.getLogger(__name__)
 
 
 class StatsRepository(DynamoBase):
-    conf = {
-        'aws_access_key_id': settings.AWS_ACCESS_KEY_ID,
-        'aws_secret_access_key': settings.AWS_SECRET_ACCESS_KEY,
-        'region_name': settings.AWS_REGION
-    }
 
     def __init__(self, conf):
         self.conf = conf
